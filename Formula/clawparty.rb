08c72367b37851eee78ce34915439e21c396b43f
caishu <booker.cai97@gmail.com>
2026-04-01 09:04:42 +0800

    Update to version 0.3.2

--- Formula/clawparty.rb
+++ Formula/clawparty.rb
@@ -1,8 +1,8 @@
 class Clawparty < Formula
   desc "Privacy-first, chat-native multi-agent platform"
   homepage "https://github.com/clawparty-ai/clawparty"
-  url "https://github.com/clawparty-ai/clawparty.git", tag: "v0.3.1"
-  version "0.3.1"
+  url "https://github.com/clawparty-ai/clawparty.git", tag: "v0.3.2"
+  version "0.3.2"
 
   depends_on "cmake" => :build
   depends_on "node" => :build
