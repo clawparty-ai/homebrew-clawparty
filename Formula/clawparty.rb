651def4fabec8e4d4c1c00c2e867b8bb7a61a3ba
caishu <booker.cai97@gmail.com>
2026-04-06 12:01:47 +0800

    Update to version 0.4.1

--- Formula/clawparty.rb
+++ Formula/clawparty.rb
@@ -1,11 +1,12 @@
 class Clawparty < Formula
   desc "Privacy-first, chat-native multi-agent platform"
   homepage "https://github.com/clawparty-ai/clawparty"
-  url "https://github.com/clawparty-ai/clawparty.git", tag: "v0.3.2"
-  version "0.3.2"
+  url "https://github.com/clawparty-ai/clawparty.git", tag: "v0.4.1"
+  version "0.4.1"
 
   depends_on "cmake" => :build
   depends_on "node" => :build
+  depends_on "rust" => :build
 
   def install
     system "./build.sh"
